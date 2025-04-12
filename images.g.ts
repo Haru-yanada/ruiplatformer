16d68c9a5b2e4f727926d0bc1ef43a4b6bbc90fb
Haru-yanada <mitsuonouta2@yahoo.co.jp>
2025-04-12 15:24:55 -0600

    MakeCodeプロジェクトの初期ファイル

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "るい":return img`
+. . . . . f f f f . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f f f f f f f f . . . . 
+. . f f f f f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . f d d d d d f . . . . . 
+. . . . f d f d f d f . . . . . 
+. . . . f d d d d d f . . . . . 
+. . f f f d f f f d f f f . . . 
+. . f d d d d d d d d d f . . . 
+. . f f f f d d d f f f f . . . 
+. . . . . f d d d f . . . . . . 
+. . . . f f d d d f f . . . . . 
+. . . f f d d d d d f f . . . . 
+. . . f d d f f f d d f . . . . 
+. . . f f f f . f f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
